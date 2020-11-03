3d124aa34a0f0ddf22d9f0190466855d683347b1
Sara A <calisaraalhindi@gmail.com>
2020-11-03 03:45:52 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    @analyzed_text = Text_Analyzer.new(params[:user_text])
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
 
     erb :results
   end
